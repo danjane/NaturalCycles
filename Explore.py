337b613e67d6687f3f92180bad5aee1df2fbd6da
danjane <jdtjane@gmail.com>
2018-04-15 13:40:23 +0200

    Tried a basic Lasso on the numerical data

--- Explore.py
+++ Explore.py
@@ -1,7 +1,17 @@
 import random
 import pandas as pd
+import numpy as np
+import sklearn.linear_model
 
 data_file = './Data/anafile_challenge_170522.csv'
+
+numerical_columns = ['Age', 'NumBMI', 'TempLogFreq', 'SexLogFreq', 'AnovCycles ']
+
+def get_X_Y_from_df(df):
+    Y = df['CyclesTrying'].as_matrix()
+    X = df[numerical_columns].as_matrix()
+    X = np.concatenate([X, X ** 2], axis=1)  # basic non-linear model
+    return X, Y
 
 # Use the same seed each time for repeatable results
 same_random_seed = int.from_bytes('nc'.encode('utf-8'), 'little')  # cutesy
@@ -18,3 +28,29 @@ if __name__ == "__main__":
     print("Checking the data, starting with numerical data:")
     print(data.describe().round(1))
     print(data.describe(include=object))
+
+    print("Have a go for time to preg")
+    data_preg = data[data['ExitStatus'] == 'Pregnant']
+    data_train = data_preg.sample(frac=0.75, random_state=same_random_seed)
+    data_validate = data_preg.drop(data_train.index)
+
+    X_train, Y_train = get_X_Y_from_df(data_train)
+    X_validate, Y_validate = get_X_Y_from_df(data_validate)
+
+    regr = sklearn.linear_model.Lasso()
+    alphas = np.logspace(-6, 3, 20)
+    scores = [regr.set_params(alpha=alpha, max_iter=10000, normalize=True
+                              ).fit(X_train, Y_train
+                                    ).score(X_validate, Y_validate)
+              for alpha in alphas]
+    rsq = max(scores)
+    best_alpha = alphas[scores.index(rsq)]
+    regr.set_params(alpha=best_alpha, max_iter=10000, normalize=True)
+    regr.fit(X_train, Y_train)
+    print("validation r^2 is {:.1f}%".format(rsq*100))
+    print(regr.coef_)
+
+    new_shape = (2, len(numerical_columns))
+    modelL1 = pd.DataFrame(data=regr.coef_.reshape(new_shape), columns=numerical_columns)
+
+    print(modelL1)
--- README.md
+++ README.md
@@ -8,4 +8,15 @@ numerical looks as expected, as does textual
     top         SE      False       normal      regular    Dropout
     freq     11864      10189       10165        8212       7651
 
+Note, before we go any further I'm going to set aside 20% of the data until the very end.
+This is because I'm going to try to make some predictions, so this test data will give me 
+an idea of the out-of-sample power of my models.
+
+Let's check the basics - try a linear regression for time to pregnancy on just 
+the numerical stuff. This will check the X predicting y pipes, and we can use a L1 
+penalty to immediately enforce scarcity. Coefficients found:
+
+            Age    NumBMI  TempLogFreq  SexLogFreq  AnovCycles 
+           0.0  0.000000    -0.613246   -5.892810     2.070549
+           0.0 -0.000057    -1.926542    1.261366    -0.083681
 
